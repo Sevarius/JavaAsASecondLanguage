3523ab022d21874e9ae2f02c74ea3ca030d4a0d5
Sevarius <43248695+Sevarius@users.noreply.github.com>
2020-09-23 21:21:30 +0300

    my

--- lecture02/src/main/java/io/github/javaasasecondlanguage/lecture02/practice/tree/quant/QuantTreeImpl.java
+++ lecture02/src/main/java/io/github/javaasasecondlanguage/lecture02/practice/tree/quant/QuantTreeImpl.java
@@ -10,6 +10,25 @@ public class QuantTreeImpl extends AbstractTree<Integer> implements QuantTree {
 
     @Override
     public int getMaxHeight() {
-        throw new RuntimeException("Not Implemented");
+        var root = this.getRoot();
+        QuantTreeImpl.maxHeight = 0;
+        recInnerCheck(root, 1);
+        return QuantTreeImpl.maxHeight;
+    }
+
+    public static int maxHeight = 0;
+
+    public void recInnerCheck(TreeNode<Integer> root, int height) {
+        if(root == null) {
+            QuantTreeImpl.maxHeight = Math.max(height-1, QuantTreeImpl.maxHeight);
+            return;
+        }
+        if(root == null || root.getChildren() == null || root.getChildren().size() == 0) {
+            QuantTreeImpl.maxHeight = Math.max(height, QuantTreeImpl.maxHeight);
+            return;
+        }
+        for (var node : root.getChildren()) {
+            recInnerCheck(node, height + 1);
+        }
     }
 }
--- lecture02/src/test/java/io/github/javaasasecondlanguage/lecture02/practice/tree/quant/QuantTreeImplTest.java
+++ lecture02/src/test/java/io/github/javaasasecondlanguage/lecture02/practice/tree/quant/QuantTreeImplTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled
+//@Disabled
 class QuantTreeImplTest {
 
     @Test
